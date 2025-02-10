f6bfce5c14d618d27d2489100a042f201b39b481
Sarah Walling-Bell <sarah.wallingbell@alleninstitute.org>
2025-02-10 11:41:27 -0800

    update path to script

--- cell_report_card/run_cell_report_card.bat
+++ cell_report_card/run_cell_report_card.bat
@@ -1,4 +1,4 @@
 @echo off
 call C:\Users\sarah.wallingbell\AppData\Local\anaconda3\Scripts\activate.bat morph_utils_v3
-python \\allen\programs\celltypes\workgroups\mousecelltypes\SarahWB\github_projects\cell_report_card\cell_report_card.py
+python \\allen\programs\celltypes\workgroups\mousecelltypes\SarahWB\github_projects\cell_report_card\cell_report_card\cell_report_card.py
 call conda deactivate
